8c69506a362decd0fdc0d374b602630dd1e5c90d
林政浩 <1392872316@qq.com>
2022-10-11 22:53:54 +0800

    添加项目文件。

---
+++ badcount.c
@@ -0,0 +1,19 @@
+/* badcount.c -- 参数错误的情况 */
+#include <stdio.h>
+int main(void) {
+	int n = 4;
+	int m = 5;
+	float f = 7.0f;
+	float g = 8.0f;
+
+	printf("%d\n", n, m);	//参数太多
+	printf("%d %d %d\n", n);	//参数太少
+	printf("%d %d\n", f, g);	//值的类型不匹配
+
+	return 0;
+}
+
+//运行结果：
+//4
+//4 2953251 2953251
+//0 1075576832
